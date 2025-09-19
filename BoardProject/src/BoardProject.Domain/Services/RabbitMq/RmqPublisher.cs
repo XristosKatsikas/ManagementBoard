ebec7f1010e133a837813fab3b6c43a5f0d70ab3
Christos Katsikas <katsikasxr@gmail.com>
2025-09-19 16:31:59 +0300

    refactor publishing

--- BoardProject/src/BoardProject.Domain/Services/RabbitMq/RmqPublisher.cs
+++ BoardProject/src/BoardProject.Domain/Services/RabbitMq/RmqPublisher.cs
@@ -12,21 +12,43 @@ namespace BoardProject.Domain.Services.RabbitMq
         private readonly ConnectionFactory _eventBusConnectionFactory;
         private readonly EventBusSettings _settings;
 
+        private const string fanoutExchange = "fanout";
+
         public RmqPublisher(EventBusSettings settings, ConnectionFactory eventBusConnectionFactory)
         {
             _settings = settings;
             _eventBusConnectionFactory = eventBusConnectionFactory;
             _connection = _eventBusConnectionFactory.CreateConnection();
             _channel = _connection.CreateModel();
-            _channel.QueueDeclare(queue: _settings.EventQueue, true, false);
+            // Declare the fanout exchange if not exists
+            _channel.ExchangeDeclare(exchange: fanoutExchange, type: ExchangeType.Fanout);
+            // Enable publisher confirms
+            _channel.ConfirmSelect();
         }
 
         public async Task PublishAsync<T>(T @event) where T : class
         {
             var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event));
-            _channel.ConfirmSelect();
-            await Task.Run(() => _channel.BasicPublish(exchange: "", routingKey: _settings.EventQueue, basicProperties: null, body: body));
-            _channel.WaitForConfirmsOrDie();
+
+            await Task.Run(() =>
+            {
+                _channel.BasicPublish(
+                    exchange: fanoutExchange,
+                    routingKey: "",
+                    basicProperties: null,
+                    body: body);
+            });
+
+            // Wait for confirmation
+            try
+            {
+                _channel.WaitForConfirmsOrDie();
+            }
+            catch (Exception ex)
+            {
+                // Handle exception (logging, retries, etc.)
+                throw new ApplicationException("Message publish failed.", ex);
+            }
         }
 
         public void Dispose()
